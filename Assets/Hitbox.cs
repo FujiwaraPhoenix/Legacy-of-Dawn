5f45bf7488ff8e7d12f589b5b60decdcfcacbc14
Unknown <heianhourai@gmail.com>
2017-03-19 21:16:58 -0400

    Woooo sprites for more stuff! \o/

---
+++ Assets/Hitbox.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hitbox : MonoBehaviour {
+    public bool on;
+    public Controller cont;
+    public SpriteRenderer spriteRenderer;
+    public Sprite a;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!Controller.Instance.paused)
+        {
+            onoff();
+        }
+        }
+
+    void onoff()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            on = true;
+        }
+        else
+        {
+            on = false;
+        }
+        if (!on)
+        {
+            spriteRenderer.enabled = false;
+        }
+        else
+        {
+            spriteRenderer.enabled = true;
+        }
+    }
+}
