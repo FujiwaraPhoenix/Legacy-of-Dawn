3bf676531ca763237ff666d5c6a567ef936e11c2
Unknown <heianhourai@gmail.com>
2017-04-29 15:00:43 -0400

    Time to implement everything. Dawn of the first day.

---
+++ Assets/Sigil.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sigil : MonoBehaviour {
+    Vector3 rotate, rotate2;
+
+	// Use this for initialization
+	void Start () {
+        rotate2 = new Vector3(0, 0, -.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(rotate2);
+	}
+}
